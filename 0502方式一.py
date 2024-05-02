6de2d9da357c2e3d8e58286c78ab3d4b3d0dc007
Doooot0512 <166679339+Doooot0512@users.noreply.github.com>
2024-05-02 11:17:42 +0800

    0502-1-100奇數和

---
+++ 0502方式一.py
@@ -0,0 +1,9 @@
+
+sum = 0 
+for x in range(1, 101): 
+    if x % 2 ==0:
+        continue
+    sum = sum + x 
+
+print(sum) 
+print("409012466 許錫典")
---
+++ 0502方式二.py
@@ -0,0 +1,7 @@
+sum = 0 
+for x in range(1, 101): 
+    if x % 2 !=0:
+     sum = sum + x 
+
+print(sum) 
+print("409012466 許錫典")
